1213414ca68eda9e7c0d05337ee860a1faceec47
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-02-06 16:08:18 +0700

    #07 Higher Order Functions

---
+++ 07-HOF/index.js
@@ -0,0 +1,7 @@
+/**
+ * Higher Order Functions
+ * HOF are functions that operate on other functions, either by taking them as arguments or by returning them
+ */
+
+// const multiply = a => b => a * b;
+// console.log(multiply(2)(4));
